917bdf6dc6875f137e0c7b4c9b31e6c172992f60
joseymartine <josephinemartine503@gmail.com>
2025-08-11 15:04:33 +0300

    CourseManagementinitialize

---
+++ CourseManagementService/src/test/java/com/endesha360/CourseManagementService/CourseManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endesha360.CourseManagementService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
