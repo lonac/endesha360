85845e77a7555625ea1088601c4b8c9d66818b18
FranciskaMatiko <francikamatiko@gmail.com>
2025-08-16 20:41:21 +0300

    small changes on service registry main app

--- service-registry/src/main/java/com/endesha360/service_registry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/endesha360/service_registry/ServiceRegistryApplication.java
@@ -2,6 +2,7 @@ package com.endesha360.service_registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
